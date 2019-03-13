04833f8eddc2d59fe3dba92e22434720f7569db6
Plaban J Rajkhowa <plabanj@geekyants.com>
2019-03-13 17:28:31 +0530

    Counters added

--- todo_api/mark.php
+++ todo_api/mark.php
@@ -3,8 +3,11 @@ header("Access-Control-Allow-Origin: *");
 
 include "connect.php";
 $id = $_REQUEST[id];
+$checked = $_REQUEST[checked];
 
-$sql = "UPDATE task SET is_completed = '1' WHERE task.id = $id";
+$sql = "UPDATE task SET is_completed = $checked WHERE task.id = $id";
+
+
 $result = mysqli_query($conn,$sql);
 
 if($result){
